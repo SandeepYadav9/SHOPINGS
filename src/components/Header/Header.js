24b2749b84f1757a95469df72f021c37fb3a2c38
Sandeep Yadav <sandeepuim10@gmail.com>
2022-07-21 11:05:35 +0530

    HEADER-CSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Header.css";
 import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
-    <header>
+    <header className="main-header">
       <nav>
         <ul>
           <li>
